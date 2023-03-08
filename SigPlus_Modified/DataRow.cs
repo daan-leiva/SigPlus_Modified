28ef1c19e080b51092bbf831699ec5167904aeb7
Daan Leiva <dleiva@ati.local>
2023-03-08 04:03:58 -0800

    Migrated data connection to an sql server instead of access

--- SigPlus_Modified/DataRow.cs
+++ SigPlus_Modified/DataRow.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace SigPlus_Modified
 {
-   struct DataRow
-   {
-      public string name { get;  set; }
-      public string companyName { get; set; }
-      public DateTime timeStamp { get; set; }
-      public bool citizenOrResident { get; set; }
-      public string visitee { get; set; }
-      public string otherPerson { get; set; }
-      public string chaperone { get; set; }
-   }
+    struct DataRow
+    {
+        public string name { get; set; }
+        public string companyName { get; set; }
+        public DateTime timeStamp { get; set; }
+        public bool citizenOrResident { get; set; }
+        public string visitee { get; set; }
+        public string otherPerson { get; set; }
+        public string chaperone { get; set; }
+    }
 }
--- SigPlus_Modified/viewRow.cs
+++ SigPlus_Modified/viewRow.cs
@@ -7,25 +7,25 @@ using System.Drawing;
 
 namespace SigPlus_Modified
 {
-   class viewRow
-   {
-      public DateTime timeStamp { get; set; }
-      public Image name { get; set; }
-      public Image company { get; set; }
-      public bool citizenOrResident { get; set; }
-      public string visitee { get; set; }
-      public Image otherPerson { get; set; }
-      public string chaperone { get; set; }
+    class viewRow
+    {
+        public DateTime timeStamp { get; set; }
+        public Image name { get; set; }
+        public Image company { get; set; }
+        public bool citizenOrResident { get; set; }
+        public string visitee { get; set; }
+        public Image otherPerson { get; set; }
+        public string chaperone { get; set; }
 
-      public viewRow(DateTime _timeStamp, Image _name, Image _company, bool _citizenOrResident, string _visitee, Image _otherPerson, string _chaperone)
-      {
-         timeStamp = _timeStamp;
-         name = _name;
-         company = _company;
-         citizenOrResident = _citizenOrResident;
-         visitee = _visitee;
-         otherPerson = _otherPerson;
-         chaperone = _chaperone;
-      }
-   }
+        public viewRow(DateTime _timeStamp, Image _name, Image _company, bool _citizenOrResident, string _visitee, Image _otherPerson, string _chaperone)
+        {
+            timeStamp = _timeStamp;
+            name = _name;
+            company = _company;
+            citizenOrResident = _citizenOrResident;
+            visitee = _visitee;
+            otherPerson = _otherPerson;
+            chaperone = _chaperone;
+        }
+    }
 }
